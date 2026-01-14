1e37caa072d298e4d6aa59269aae9cb480ded666
phmucelin <contatopedromucelin05@gmail.com>
2026-01-13 23:03:59 -0300

    Feat: Melhorias Program API

--- Program.cs
+++ Program.cs
@@ -14,6 +14,16 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 builder.Services.AddEndpointsApiExplorer(); 
 builder.Services.AddSwaggerGen();           
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -23,15 +33,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(); 
 }
 
-
-List<Wallet> bancoDeDados = new List<Wallet>();
-
-
-var c1 = new Wallet("Pedro");
-bancoDeDados.Add(c1);
-Console.WriteLine($"PEDRO ID: {c1.Id}"); 
-
-
+app.UseCors("AllowAll");
 app.MapWalletEndpoints();
 
 app.Run();
